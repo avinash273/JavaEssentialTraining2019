08fab5375a5c29473f7b2edecf1a36a430b23a06
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-14 16:12:52 -0500

    minor changes, Java 13 required not working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+/**
+ * JDK version 13 required, I am currently on java11 hence not working correctly
+ */
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         var line = "Hi I am avinash shanker";
         System.out.println(line.indexOf("am"));
+        System.out.println(line.substring(29));
     }
 }
