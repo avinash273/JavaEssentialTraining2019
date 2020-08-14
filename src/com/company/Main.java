239b9e25904cab57c60b33dc9dfed7ec669c37b2
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-14 15:59:19 -0500

    minor changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var line = "Hi I am avinash shanker";
+        System.out.println(line.indexOf("am"));
     }
 }
